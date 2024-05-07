d818b269f7e31d497977bf713575ba0345da7837
shillari <sandy_hmh2@hotmail.com>
2024-05-07 16:02:25 +0200

    configured cors to allow netlify

--- index.js
+++ index.js
@@ -28,8 +28,18 @@ let auth = require('./config/auth')(app);
 const passport = require('passport');
 require('./config/passport');
 const cors = require('cors');
-let allowedOrigins = ['*'];
-app.use(cors());
+let allowedOrigins = ['http://localhost:1234', 'https://main--moonflix-app.netlify.app'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use((err, req, res, next) => {
